88ac00d4b4fe980e66a149b5310985d9a1df871d
XueGao <zihang518@outlook.com>
2026-03-15 15:13:55 +0800

    Update constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,8 @@
 export const PAGE_SIZE = 8;
 
 export const DARK_MODE = "dark";
+export const LIGHT_MODE = "light";
+export const AUTO_MODE = "auto";
 
 // Banner height unit: vh
 export const BANNER_HEIGHT = 35;
